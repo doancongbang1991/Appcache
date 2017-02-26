be9c8ec1a194ca23331c808f595a11ca131d2f8e
Bang Doan <doancongbang1991@gmail.com>
2017-02-26 09:01:21 +0700

    Update code

---
+++ Appcache/AppCacheMain.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Appcache
+{
+    partial class AppCacheMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_link = new System.Windows.Forms.Label();
+            this.tb_lnk = new System.Windows.Forms.TextBox();
+            this.bt_open = new System.Windows.Forms.Button();
+            this.treeview_open = new System.Windows.Forms.TreeView();
+            this.bt_crt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb_link
+            // 
+            this.lb_link.AutoSize = true;
+            this.lb_link.Location = new System.Drawing.Point(13, 9);
+            this.lb_link.Name = "lb_link";
+            this.lb_link.Size = new System.Drawing.Size(27, 13);
+            this.lb_link.TabIndex = 0;
+            this.lb_link.Text = "Link";
+            // 
+            // tb_lnk
+            // 
+            this.tb_lnk.Location = new System.Drawing.Point(55, 5);
+            this.tb_lnk.Name = "tb_lnk";
+            this.tb_lnk.Size = new System.Drawing.Size(443, 20);
+            this.tb_lnk.TabIndex = 1;
+            // 
+            // bt_open
+            // 
+            this.bt_open.Location = new System.Drawing.Point(504, 4);
+            this.bt_open.Name = "bt_open";
+            this.bt_open.Size = new System.Drawing.Size(75, 23);
+            this.bt_open.TabIndex = 2;
+            this.bt_open.Text = "Open Index";
+            this.bt_open.UseVisualStyleBackColor = true;
+            this.bt_open.Click += new System.EventHandler(this.bt_open_Click);
+            // 
+            // treeview_open
+            // 
+            this.treeview_open.Location = new System.Drawing.Point(12, 32);
+            this.treeview_open.Name = "treeview_open";
+            this.treeview_open.Size = new System.Drawing.Size(648, 280);
+            this.treeview_open.TabIndex = 3;
+            // 
+            // bt_crt
+            // 
+            this.bt_crt.Location = new System.Drawing.Point(585, 4);
+            this.bt_crt.Name = "bt_crt";
+            this.bt_crt.Size = new System.Drawing.Size(75, 23);
+            this.bt_crt.TabIndex = 4;
+            this.bt_crt.Text = "Create File";
+            this.bt_crt.UseVisualStyleBackColor = true;
+            this.bt_crt.Click += new System.EventHandler(this.bt_crt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 324);
+            this.Controls.Add(this.bt_crt);
+            this.Controls.Add(this.treeview_open);
+            this.Controls.Add(this.bt_open);
+            this.Controls.Add(this.tb_lnk);
+            this.Controls.Add(this.lb_link);
+            this.Name = "Form1";
+            this.Text = "Create the Appcache manifest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_link;
+        private System.Windows.Forms.TextBox tb_lnk;
+        private System.Windows.Forms.Button bt_open;
+        private System.Windows.Forms.TreeView treeview_open;
+        private System.Windows.Forms.Button bt_crt;
+    }
+}
+
---
+++ Appcache/AppCacheMain.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Appcache
+{
+    public partial class AppCacheMain : Form
+    {
+        public AppCacheMain()
+        {
+            InitializeComponent();
+        }
+        //COnfig default value
+        public static string strfilename = "";
+        public static string thepath = "";
+        public static string android_path = "android";
+        public static string ios_path = "iosproj";
+        public static string appcache_filename = "offline.appcache";
+        //End of config
+
+        private void bt_open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            // Default folder
+            openFileDialog.InitialDirectory = "D:\\";
+            // Default filter
+            openFileDialog.Filter = "HTML File (*.html)|*.html|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                strfilename = openFileDialog.FileName;
+                thepath = Path.GetDirectoryName(strfilename);
+                tb_lnk.Text = strfilename;
+                ListDirectory(treeview_open, thepath);
+            }
+        }
+        private void ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Nodes.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            treeView.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
+        }
+
+        private static TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeNode(directoryInfo.Name);
+            foreach (var directory in directoryInfo.GetDirectories())
+                directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+            foreach (var file in directoryInfo.GetFiles())
+                directoryNode.Nodes.Add(new TreeNode(file.Name));
+
+            return directoryNode;
+        }
+
+        private void bt_crt_Click(object sender, EventArgs e)
+        {
+            string[] filenames = Directory.GetFiles(thepath, "*", SearchOption.AllDirectories);
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(thepath + @"\" + appcache_filename))
+            {
+                file.WriteLine(@"CACHE MANIFEST");
+                foreach (string filename in filenames)
+                {
+                    string filename_tmp = filename.Remove(0, thepath.Length + 1);
+                    string filename_tmpnew = filename_tmp.Replace(@"\", @"/");
+                    string filename_tmpnew1 = filename_tmpnew.Replace(@" ", @"%20");
+                    //remove android & ios subfoler source code from appcache
+                    if (filename_tmpnew1.Contains(android_path + @"/") || filename_tmpnew1.Contains(ios_path + @"/"))
+                        continue;
+                    file.WriteLine(filename_tmpnew1);
+                }
+                file.WriteLine(@"FALLBACK:");
+                //Default offline using html file
+                file.WriteLine(@"/ /offline.html");
+                file.WriteLine(@"NETWORK:");
+                file.WriteLine(@"*");
+            }
+        }
+    }
+}
